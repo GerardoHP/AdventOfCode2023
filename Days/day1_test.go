8343e54570cedeac8973914ae8230a8e912c2bdf
Gerardo Hernandez <gerardo@hernandez.perales.dev>
2023-12-06 10:26:35 -0600

    add support for part 2

--- Days/day1_test.go
+++ Days/day1_test.go
@@ -20,3 +20,25 @@ func TestDay1(t *testing.T) {
 		t.Fatalf("expected %v, but found %v \n", expected, result)
 	}
 }
+
+func TestDay1_part2(t *testing.T) {
+	// Arrange
+	codes := []string{
+		"two1nine",
+		"eightwothree",
+		"abcone2threexyz",
+		"xtwone3four",
+		"4nineeightseven2",
+		"zoneight234",
+		"7pqrstsixteen",
+	}
+	expected := 281
+
+	// Act
+	result := Day1(codes)
+
+	// Assert
+	if result != expected {
+		t.Fatalf("expected %v, but found %v \n", expected, result)
+	}
+}
